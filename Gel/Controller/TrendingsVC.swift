ae9911ab13c36c2ea9310c360cc9bf798ab4598d
furrki-Mac <admin@Admins-Mac.local>
2019-02-03 01:52:50 +0300

    Example Table

--- Gel/Controller/TrendingsVC.swift
+++ Gel/Controller/TrendingsVC.swift
@@ -9,13 +9,7 @@
 import UIKit
 
 class TrendingsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
-    }
     
 
     @IBOutlet weak var tableView: UITableView!
@@ -24,8 +18,19 @@ class TrendingsVC: UIViewController, UITableViewDelegate, UITableViewDataSource 
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.reloadData()
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TrendingCell", for: indexPath) as? TrendingCell else { return UITableViewCell() }
+        let repo = Repo(image: UIImage(named: "search")!,  name: "Swift", description: "Apple's Programming Lang.", numberOfForks: 388, language: "Swift", numberOfContributors: 1453, repoUrl: "http://furrki.com")
+        cell.configCell(repo: repo)
+        return cell
+    }
 
 }
 
